2f0d96d9bb5b95927ed95bcf10dd3c83b5296884
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2023-07-17 19:13:58 +0000

    Extend pattern detection for multiple colors at the same time

--- src/pattern.rs
+++ src/pattern.rs
@@ -4,7 +4,7 @@ use std::marker::PhantomData;
 #[derive(Debug, PartialEq, Eq)]
 pub enum SearchStep {
     Reject(Point),
-    Match(Point),
+    Match(Color, Point),
     Done
 }
 
@@ -24,13 +24,13 @@ pub struct SearcherIter<'a, S: Searcher<'a>> {
 }
 
 impl<'a, S: Searcher<'a>> Iterator for SearcherIter<'a, S> {
-    type Item = Point;
+    type Item = (Color, Point);
 
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             match self.searcher.next() {
                 SearchStep::Done => { return None; },
-                SearchStep::Match(at) => { return Some(at); },
+                SearchStep::Match(color, at) => { return Some((color, at)); },
                 _ => {},
             }
         }
@@ -50,7 +50,6 @@ impl<'a, S: Searcher<'a>> Iterator for SearcherIter<'a, S> {
 /// operations when stepping forward.
 pub struct EyeSearcher<'a> {
     goban: &'a Goban,
-    color: Color,
     finger: (u8, u8),
     pattern: u16,
 }
@@ -89,27 +88,52 @@ impl<'a> Searcher<'a> for EyeSearcher<'a> {
                 // x x x
                 // x   x
                 // x x x
-                0xffff => SearchStep::Match((x, y).into()),
+                0xffff => SearchStep::Match(Color::Black, (x, y).into()),
 
                 //   x x
                 // x   x
                 // x x x
-                0x3fff | 0xf3ff | 0xffcf | 0xfffc => SearchStep::Match((x, y).into()),
+                0x3fff | 0xf3ff | 0xffcf | 0xfffc => SearchStep::Match(Color::Black, (x, y).into()),
 
                 // o x x
                 // x   x
                 // x x x
-                0xbfff | 0xfbff | 0xfffe | 0xffef => SearchStep::Match((x, y).into()),
+                0xbfff | 0xfbff | 0xfffe | 0xffef => SearchStep::Match(Color::Black, (x, y).into()),
 
                 // - - -
                 // x   x
                 // x x x
-                0xf77d | 0xffd5 | 0x7ddf | 0x57ff => SearchStep::Match((x, y).into()),
+                0xf77d | 0xffd5 | 0x7ddf | 0x57ff => SearchStep::Match(Color::Black, (x, y).into()),
 
                 // - - -
                 // -   x
                 // - x x
-                0xf755 | 0x7dd5 | 0x55df | 0x577d => SearchStep::Match((x, y).into()),
+                0xf755 | 0x7dd5 | 0x55df | 0x577d => SearchStep::Match(Color::Black, (x, y).into()),
+
+                // o o o
+                // o   o
+                // o o o
+                0xaaaa => SearchStep::Match(Color::White, (x, y).into()),
+
+                //   o o
+                // o   o
+                // o o o
+                0x2aaa | 0xa2aa | 0xaa8a | 0xaaa8 => SearchStep::Match(Color::White, (x, y).into()),
+
+                // x o o
+                // o   o
+                // o o o
+                0xeaaa | 0xaeaa | 0xaaa9 | 0xa6aa => SearchStep::Match(Color::White, (x, y).into()),
+
+                // - - -
+                // o   o
+                // o o o
+                0x699a | 0xaa96 | 0xa669 | 0x56aa => SearchStep::Match(Color::White, (x, y).into()),
+
+                // - - -
+                // -   o
+                // - o o
+                0x559a | 0x6995 | 0xa655 | 0x5669 => SearchStep::Match(Color::White, (x, y).into()),
 
                 _ => SearchStep::Reject((x, y).into())
             }
@@ -118,7 +142,7 @@ impl<'a> Searcher<'a> for EyeSearcher<'a> {
 }
 
 impl<'a> IntoIterator for EyeSearcher<'a> {
-    type Item = Point;
+    type Item = (Color, Point);
     type IntoIter = SearcherIter<'a, EyeSearcher<'a>>;
 
     fn into_iter(self) -> Self::IntoIter {
@@ -137,8 +161,8 @@ impl<'a> EyeSearcher<'a> {
         } else {
             match self.goban.at((x, y).into()) {
                 None => 0,
-                Some(color) if color == self.color => 3,
-                Some(_) => 2,
+                Some(Color::Black) => 3,
+                Some(Color::White) => 2,
             }
         }
     }
@@ -156,13 +180,11 @@ impl<'a> EyeSearcher<'a> {
     }
 }
 
-pub struct Eye {
-    color: Color,
-}
+pub struct Eye;
 
 impl Eye {
-    pub fn new(color: Color) -> Self {
-        Self { color }
+    pub fn new() -> Self {
+        Self { }
     }
 }
 
@@ -172,7 +194,6 @@ impl<'a> Pattern<'a> for Eye {
     fn into_searcher(self, goban: &'a Goban) -> Self::Searcher {
         EyeSearcher {
             goban,
-            color: self.color,
             finger: (0, 0),
             pattern: 0,
         }
@@ -191,7 +212,7 @@ mod tests {
     /// x x
     /// ```
     #[test]
-    fn eye_detects_eyes() {
+    fn eye_detects_black_eyes() {
         let mut goban = Goban::new(9, 9);
         goban.play((0u8, 0u8).into(), Color::Black);
         goban.play((0u8, 1u8).into(), Color::Black);
@@ -214,12 +235,53 @@ mod tests {
         goban.play((8u8, 1u8).into(), Color::Black);
 
         assert_eq!(
-            Eye::new(Color::Black).into_searcher(&goban).into_iter().collect::<Vec<_>>(),
+            Eye::new().into_searcher(&goban).into_iter().collect::<Vec<_>>(),
             vec! [
-                (8u8, 0u8).into(),
-                (1u8, 1u8).into(),
-                (3u8, 2u8).into(),
-                (0u8, 3u8).into(),
+                (Color::Black, (8u8, 0u8).into()),
+                (Color::Black, (1u8, 1u8).into()),
+                (Color::Black, (3u8, 2u8).into()),
+                (Color::Black, (0u8, 3u8).into()),
+            ]
+        );
+    }
+
+    /// ```
+    /// x x x       x
+    /// x   x x     x x
+    /// x x x   x
+    ///   x x x x
+    /// x x
+    /// ```
+    #[test]
+    fn eye_detects_white_eyes() {
+        let mut goban = Goban::new(9, 9);
+        goban.play((0u8, 0u8).into(), Color::White);
+        goban.play((0u8, 1u8).into(), Color::White);
+        goban.play((0u8, 2u8).into(), Color::White);
+        goban.play((0u8, 4u8).into(), Color::White);
+        goban.play((1u8, 0u8).into(), Color::White);
+        goban.play((1u8, 2u8).into(), Color::White);
+        goban.play((1u8, 3u8).into(), Color::White);
+        goban.play((1u8, 4u8).into(), Color::White);
+        goban.play((2u8, 0u8).into(), Color::White);
+        goban.play((2u8, 1u8).into(), Color::White);
+        goban.play((2u8, 2u8).into(), Color::White);
+        goban.play((2u8, 3u8).into(), Color::White);
+        goban.play((3u8, 1u8).into(), Color::White);
+        goban.play((3u8, 3u8).into(), Color::White);
+        goban.play((4u8, 2u8).into(), Color::White);
+        goban.play((4u8, 3u8).into(), Color::White);
+        goban.play((7u8, 0u8).into(), Color::White);
+        goban.play((7u8, 1u8).into(), Color::White);
+        goban.play((8u8, 1u8).into(), Color::White);
+
+        assert_eq!(
+            Eye::new().into_searcher(&goban).into_iter().collect::<Vec<_>>(),
+            vec! [
+                (Color::White, (8u8, 0u8).into()),
+                (Color::White, (1u8, 1u8).into()),
+                (Color::White, (3u8, 2u8).into()),
+                (Color::White, (0u8, 3u8).into()),
             ]
         );
     }
